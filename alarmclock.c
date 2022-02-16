3f4e77561c537f0bfb913e05fc6d496b166b0102
Daniel Yang Hansen <daniel.yang.hansen@gmail.com>
2022-02-16 16:42:44 +0100

    There's some weird shit with the description

--- alarmclock.c
+++ alarmclock.c
@@ -27,13 +27,13 @@ void ring();
 
 typedef struct {
     char functionChar;
-    char functionDescription[20];
+    char functionDescription[40];
 } FunctionDescriptor;
 
 typedef struct Alarm {
     time_t secondsLeft;
     struct tm targetTime;
-    char alarmDescription[40];
+    char alarmDescription[42];
 } alarm; //alarm is an alias for "struct Alarm"
 
 alarm _gAlarmList[10]; //TODO: GLOBAL ACCESS and fill with relevant Alarms by PID from var or tmp
@@ -49,8 +49,16 @@ void ring() {
 void outputAlarm(struct Alarm alarm) {
     long remainingSeconds = (long)alarm.secondsLeft;
     char outDateString[40] = {0};
+    char testDescString[42] = {"a"};
+    printf("test: %42c", testDescString);
+
     strftime(&outDateString, 39, "%a %m. %h %Y: %H:%M:%S", &alarm.targetTime);
-    printf("Alarm description: %s\nAlarm target time: %s\n\nRemaining time in seconds: %lu", alarm.alarmDescription, outDateString, remainingSeconds);
+    printf(
+        "Alarm description: %42c\nAlarm target time: %s\n\nRemaining time in seconds: %lu",
+        alarm.alarmDescription,
+        outDateString,
+        remainingSeconds
+    );
 }   //TODO: Check if %lu needs to be replaced with %ld
 
 int scheduleAlarm(char *targetTime) {
@@ -61,7 +69,7 @@ void f_s() {
     printf("This is the schedule function");
     //int second, minute, hour, day, month, year;
     struct tm alarmTm;
-    char timeString[20] = {0};
+    char timeString[40] = {0};
     char *arrValue;
 
     printf("Enter time in format: YYYY:MM:DD:HH:MM:SS\n");
@@ -74,19 +82,17 @@ void f_s() {
 
         invalidTime = 0;
     }
-    char descString[41] = {0};
     printf("Entered time is %s \n", timeString);
-    
+    char descString[42] = {0};
+
     printf("Enter a description (max 40 characters): \n");
-    scanf("%40c", &descString); //TODO: Make it so that you can enter less than 40 characters :/
-    printf("Description: %s", descString);
+    scanf(" %40[^\n]", &descString); 
+    printf("Desc: %s\n", descString);
 
     int PID = fork();
 
     if (PID == 0) {
         time_t secondCount = mktime(&alarmTm);
-        printf("&alarmTm : %i,%i,%i,%i", alarmTm.tm_year, alarmTm.tm_mon, alarmTm.tm_mday, alarmTm.tm_hour);
-
         //create alarm struct
         alarm freshAlarm = {
             secondCount,
